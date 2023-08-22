11a911bf8fa410e25352617e18cd15733a0835a2
JeanJouliaCode <jjeeaann2015gmail.com>
2023-08-22 22:51:56 +0200

    save

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html',
-            pages: 'docs',
-            assets: 'docs',
-		}),
+		adapter: adapter(
+            {
+                fallback: 'index.html',
+                pages: 'docs',
+                assets: 'docs',
+            }
+        ),
         paths: {
-            base: '/pointblank',
+            base: dev ? '' : process.env.BASE_PATH,
         }
 	}
 };
